befa178b0aa326bfc15abb637d2103d027fc1463
hendrik <hendrik>
2026-01-20 16:16:30 +0200

    req.params

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("node:path");
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 // static
@@ -10,10 +11,16 @@ let posts = [
   { id: 1, title: "First Post", content: "This is the first post." },
   { id: 2, title: "Second Post", content: "This is the second post." },
 ];
-
+//All
 app.get("/api/posts", (req, res) => {
   res.json(posts);
 });
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+//Single
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

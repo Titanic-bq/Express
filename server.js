fcf2d8c398a4f6b32162e2af6f4ef3ab6f3abcc4
hendrik <hendrik>
2026-01-20 16:12:16 +0200

    JSON

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname, "public", "index.html");
-});
+// static
 
-app.get("/about", (req, res) => {
-  res.sendFile(__dirname, "public", "about.html");
-});
+// app.use(express.static(path.join(__dirname, "public")));
 
+let posts = [
+  { id: 1, title: "First Post", content: "This is the first post." },
+  { id: 2, title: "Second Post", content: "This is the second post." },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });

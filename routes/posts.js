98539d488389d8b4f1759cab3ac316820e4db183
hendrik <hendrik>
2026-01-20 16:36:06 +0200

    Route files

---
+++ routes/posts.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+let posts = [
+  { id: 1, title: "First Post" },
+  { id: 2, title: "Second Post" },
+];
+
+//All
+router.get("/", (req, res) => {
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    res.status(200).json(posts.slice(0, limit));
+  } else {
+    res.status(200).json(posts);
+  }
+});
+//Single
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id);
+
+  if (!post) {
+    return res.status(404).json({ error: "Post with id not found" });
+  } else {
+    res.status(200).json(post);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,38 +1,16 @@
-const { json } = require("body-parser");
 const express = require("express");
-const path = require("node:path");
+const path = require("path");
+const posts = require("./routes/posts");
 const PORT = process.env.PORT || 8000;
+
 const app = express();
 
 // static
-
 // app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [
-  { id: 1, title: "First Post", content: "This is the first post." },
-  { id: 2, title: "Second Post", content: "This is the second post." },
-];
-//All
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
+// Rotes
 
-  if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
-  }
-});
-//Single
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((p) => p.id === id);
-
-  if (!post) {
-    return res.status(404).json({ error: "Post with id not found" });
-  } else {
-    res.status(200).json(post);
-  }
-});
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
